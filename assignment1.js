ed54095f2238a28a1dbd66842216381406267548
paridhi <parijain.iet@gmail.com>
2018-06-06 17:49:14 +0530

    assignment1 3 questions commit

--- assignment1.js
+++ assignment1.js
@@ -1,62 +1,61 @@
-// Problem 1: Complete the secondLargest function which takes in an array of numbers in input and return the second biggest number in the array. (without using sort)?
+
 function secondLargest(array) {
-  // Write your code here
-  var largest=( array[0]>array[1])?array[0]:array[1];
-  var seclargest=( array[0]>array[1])?array[1]:array[0];
-  for(var i=2;i<array.length;i++)
+  var large = ( array[0] > array[1] ) ? array[0] : array[1] ;
+  var secLarge = ( array[0] > array[1]) ? array[1] : array[0] ;
+
+  for(var i=2 ; i<array.length ; i++)
     {
-      if(array[i]>largest)
+      if(array[i] > large)
         {
-          seclargest=largest;
-          largest=array[i];
+          secLarge = large ;
+          large = array[i] ;
         }
-      else if(array[i]>seclargest && largest!=array[i])
-        seclargest=array[i];
+      else if( array[i] > secLarge && large != array[i])
+        secLarge = array[i] ;
     }
-  return seclargest;
+  return secLarge;
   
 }
 
-// Problem 2: Complete the calculateFrequency function that takes lowercase string as input and returns frequency of all english alphabet. (using only array, no in-built function)
+
+
 function calculateFrequency(string) 
-{var freq = {}
-  string.split("").forEach(function(s) {
-    if(s!=" " && s>='a' && s<='z')
-    freq[s] ? freq[s]++ : freq[s] = 1;
-  });console.log(freq);
-  return freq;}
 
-/*
-  var freq=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-  for(var i=0;i<string.length();i++)
-    {
-      freq[string[i]]++;
-    }
-  
-  var charFreq={};
- 
- for(var i=0;i<string.length();i++)
-    {
-      if(string[i]!=" " && string[i]>='a' && string[i]<='z')
-        charFreq.string[i]=freq[string[i]];
-    }
-  console.log(charFreq);
- return charFreq;
- 
-}*/
-  
-
-// Problem 3: Complete the flatten function that takes a JS Object, returns a JS Object in flatten format (compressed)
-function flatten(unflatObject) {
-  // Write your code here
-  var output={};
-  
- 
-  
+{
+	var freq = {};
+        string.split("").forEach(function(s) 
+		{
+		if(s!=" " && s>='a' && s<='z')
+		freq[s] ? freq[s]++ : freq[s] = 1;
+	 	});
+	  
+	return freq;
 }
 
-// Problem 4: Complete the unflatten function that takes a JS Object, returns a JS Object in unflatten format
-function unflatten(flatObject) {
-  // Write your code here
-}
 
+
+function flatten(unflatObject)
+{
+  
+
+	var output={};
+	for(var i in unflatObject)
+	  {
+		if(!unflatObject.hasOwnProperty(i))
+			continue;
+   
+			if((typeof unflatObject[i])==="object")
+  			{ 
+  			var flat = flatten(unflatObject[i]);
+     			for (var x in flat)
+      			{
+     				if(!flat.hasOwnProperty(x)) continue;
+     				output[i+'.'+x]=flat[x];
+      			}
+	  }
+		else 
+		output[i]= unflatObject[i];
+ 	 }
+  
+  return output;
+}
