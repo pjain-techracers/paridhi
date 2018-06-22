709f919216f847f104d2c7e09cc8e438343b4a03
paridhi <parijain.iet@gmail.com>
2018-06-22 11:44:58 +0530

    promise use

--- promise/assign4.1WithPromise.js
+++ promise/assign4.1WithPromise.js
@@ -1,41 +1,45 @@
 const fs = require('fs');
-var file1Content = ` file 1 content goes 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+let file1Content = ` file 1 content goes 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
   1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
   1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 `;
 
-var writeInFile1 = () => {
+const writeInFile1 = () => {
   return new Promise ( (resolve,reject) => {
     fs.writeFile("file1.txt", file1Content, (err,data) => {
-      if(err)
-        reject("error occured in reading file1");
+      if(err) {
+      	reject("error occured in writing file1");
+      	return;
+      }
       resolve("file 1 written initially");
     });
   });
 }
 
-var read1 = () => {
+const read1 = () => {
   return new Promise ( (resolve,reject) => {
     fs.readFile("file1.txt",(err,data) => {
-      if(err)
-        reject("error occured in reading file1");
+      if(err) {
+      	reject("error occured in reading file1");
+      	return;
+      }
       resolve("file 1 read successfull");
     });
   });
 }
 
-var append = () => {
+const append = () => {
   return new Promise((resolve,reject) => {
-    var destination = fs.createWriteStream("file1.txt", {flags: 'a'});
-    var source = fs.createReadStream("file2.txt");
+    let destination = fs.createWriteStream("file1.txt", {flags: 'a'});
+    let source = fs.createReadStream("file2.txt");
     source.pipe(destination);
     resolve("file2 appended in file 1");    
   });
 }
 
-var write = () => {
+const write = () => {
   return new Promise( (resolve,reject) => {
-    var destination = fs.createWriteStream("file3.txt", {flags: 'w'});
-    var source = fs.createReadStream("file1.txt");
+    let destination = fs.createWriteStream("file3.txt", {flags: 'w'});
+    let source = fs.createReadStream("file1.txt");
     source.pipe(destination);
     resolve("file3 written ");
   });
